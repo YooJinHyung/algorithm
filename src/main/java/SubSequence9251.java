2b36d3b6f2629e1fc42fef7feeec84f6dab05467
YooJinHyung <jin_hyung_e@naver.com>
2017-09-05 18:11:05 +0900

    [170514] 최단거리 구하기

---
+++ src/main/java/SubSequence9251.java
@@ -0,0 +1,20 @@
+//public class SubSequence9251 {
+//    public String subSequence(String sentence1, String sentence2) {
+//        int row, col;
+//        int[][] check = new int[sentence1.length()][sentence2.length()];
+//        this.init(check,sentence1.length(),sentence2.length());
+//        System.out.println(check);
+//        for (row = 0; row < sentence1.length(); row++) {
+//            for (col = 0; col < sentence2.length(); col++) {
+//                if()
+//            }
+//        }
+//    }
+//
+//    public void init(int[][] check,int row,int col){
+//        for(row-=1;row>=0;row--){
+//            for(col-=1;col>=0;col--)
+//                check[row][col]=0;
+//        }
+//    }
+//}
---
+++ src/main/java/party1238.java
@@ -0,0 +1,26 @@
+/**
+ * Created by user on 2017-05-31.
+ */
+import javafx.util.Pair;
+
+import java.util.*;
+public class party1238 {
+    Map<Integer,Pair<Integer,Integer>> townMap = new HashMap<Integer,Pair<Integer,Integer>>();
+    int distance[];
+
+    public void getDistance(){
+        int v,e,i=0;
+        int start;
+        Scanner input = new Scanner(System.in);
+        v = input.nextInt();
+        e = input.nextInt();
+        start = input.nextInt();
+        for(i=0;i<e;i++) {
+            int stv,edv,dis;
+            stv = input.nextInt();
+            edv = input.nextInt();
+            dis = input.nextInt();
+//            townMap.put(stv,new Pair<Integer,Integer>);
+        }
+    }
+}
